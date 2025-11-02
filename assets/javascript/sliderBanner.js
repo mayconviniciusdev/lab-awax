8e50dec33a0855786d035ba096657b6247aed8f3
Maycon Vinicius <contato@mayconviniciusdev.com>
2025-11-02 15:30:54 -0300

    feat: add team section and refactor slider scripts

--- assets/javascript/sliderBanner.js
+++
@@ -1,7 +0,0 @@
-const swiper = new Swiper('.swiper', {
-  loop: true,
-  slidesPerView: 1,
-  spaceBetween: 10,
-  pagination: { el: '.swiper-pagination', clickable: true},
-  autoplay: {delay: 3000,},
-});
---
+++ assets/javascript/sliders.js
@@ -0,0 +1,16 @@
+const bannerSwiper = new Swiper('.swiper-banner', {
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: { el: '.swiper-pagination', clickable: true},
+  autoplay: {delay: 3000,},
+});
+
+const teamSwiper = new Swiper('.swiper-team', {
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 20,
+  autoplay: {
+    delay: 3000,
+  },
+});
