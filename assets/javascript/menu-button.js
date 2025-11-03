9f5100729a846411e3cbeb8232d5f57053165f8e
Maycon Vinicius <contato@mayconviniciusdev.com>
2025-11-03 20:14:05 -0300

    feat: add internal link handling and mobile menu toggle

---
+++ assets/javascript/menu-button.js
@@ -0,0 +1,4 @@
+const menuBtn = document.getElementById('menu-btn');
+const nav = document.getElementById('nav');
+
+menuBtn.addEventListener('click', () => {nav.classList.toggle('open');});
---
+++ assets/javascript/smoth-scroll.js
@@ -0,0 +1,11 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const target = document.querySelector(this.getAttribute("href"));
+    if (target) window.scrollTo({top: target.offsetTop - 80, behavior: "smooth" });
+
+    const nav = document.getElementById("nav");
+    nav.classList.remove("open");
+  });
+});
